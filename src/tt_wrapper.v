e91b06921ddfb2fca770671c77be191c6fdd508c
Darryl L. Miles <darryl.miles@darrylmiles.org>
2025-08-26 14:37:55 +0100

    fixup! tt_wrapper.v: ifdef

--- src/tt_wrapper.v
+++ src/tt_wrapper.v
@@ -237,6 +237,6 @@ module tt_um_tqv_peripheral_harness (
 `endif
 
   // Ignore unused inputs
-  wire _unused = &{ena, ui_in[7:0], uio_in[7:0], 1'b0};
+  wire _unused = &{ena, uio_in[7:0], ui_in[7:0], 1'b0};
 
 endmodule
